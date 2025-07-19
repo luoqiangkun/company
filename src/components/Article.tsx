31f111872cfe9894d3cf1fc7852120fa7ad47aaa
luoqiangkun <1051211357@qq.com>
2025-07-19 08:22:33 +0800

    文章列表和详情

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,72 +1,154 @@
-
-import { IArticle, ILinkImage } from "@/lib/type";
-import Image from "next/image";
-import { strapiImage } from "@/lib/strapiImage";
+import { IArticle, ILinkImage } from '@/lib/type';
+import Image from 'next/image';
+import { strapiImage } from '@/lib/strapiImage';
 
 type Category = {
-    id:Number;
+    id: Number;
     text: string;
-}
+};
 
 type Props = {
     image: ILinkImage;
     category: Category[];
     data: IArticle[];
-}
+};
 
 export const Article = ({ image, data, category }: Props) => {
-    {{ image }}
+    let article = data[0];
     return (
-        <div className="mt-14">
-            <div className="relative w-full">
-                <Image
-                    src={strapiImage(image.image.url)}
-                    alt={image.image.alternativeText}
-                    width={1600} // 原始宽度（任意值，仅用于比例计算）
-                    height={900} // 原始高度（与宽度保持原始比例）
-                    layout="responsive"
-                />
-            </div>
+        <div className="max-w-7xl mt-14 px-4 mx-auto">
+            <section className="bg-white pt-8 mx-auto dark:bg-gray-900">
+                <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+                    新闻
+                </h1>
 
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 ">
-                <div className="flex justify-center items-center py-6 text-2xl gap-6 text-gray-800">
-                    {
-                        category.map((item,index) => {
-                           return  <span key={index}>{item.text}</span>
-                        })
-                    }
-                    
+                <div className="mt-8 lg:flex">
+                    <div className="relative lg:w-1/2 overflow-hidden bg-gray-100 rounded-lg shadow aspect-video dark:bg-gray-800">
+                        <Image
+                            className="size-full object-cover hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xs"
+                            src={strapiImage(article?.image.url)}
+                            alt={article?.image.alternativeText}
+                            fill
+                        />
+                    </div>
+
+                    <div className="relative mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
+                        <div className="flex space-x-2 items-center">
+                            <p className="text-sm uppercase">
+                                category /
+                            </p>
+                            <p className="text-neutral-500 text-sm max-w-xl group-hover:text-white transition duration-200">
+                                September 11, 2024
+                            </p>
+                        </div>
+
+                        <div>
+<a
+                            href="#"
+                            className="block mt-4 text-2xl font-semibold text-gray-800 hover:underline dark:text-white md:text-3xl"
+                        >
+                            All the features you want to know
+                        </a>
+
+                        <p className="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Iure veritatis sint autem nesciunt, laudantium
+                            quia tempore delect
+                        </p>
+                        </div>
+                        
+
+                        <a
+                            className="absolute bottom-0 inline-block text-blue-500 underline hover:text-blue-400"
+                            target="_blank"
+                        >
+                            Read more
+                        </a>
+                    </div>
                 </div>
-                <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {
-                        data.map( (item,index) => {
-                            return (
-                                <a key={index} className="group flex flex-col focus:outline-hidden" href={`/article/${item.slug}`}>
-                                    <div className="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
-                                        <Image 
-                                            className="size-full absolute top-0 start-0 object-cover group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xl" 
-                                            src={strapiImage(item?.image.url)} 
-                                            alt={item?.image.alternativeText} 
-                                            fill
-                                            />
-                                    </div>
+            </section>
 
-                                    <div className="mt-7">
-                                        <h3 className="text-ml font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
-                                        { item.title}
-                                        </h3>
-                                        <p className="mt-3 text-gray-800 dark:text-neutral-200">
-                                        { item.description }
+            <section className="grid mt-8 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                {data.map((item, index) => {
+                    return (
+                        <div key={`div-${index}`} className="py-4">
+                            <a
+                                key={index}
+                                className="block focus:outline-hidden"
+                                href={`/article/${item.slug}`}
+                            >
+                                <div className="relative w-full overflow-hidden bg-gray-100 rounded-lg shadow aspect-[16/12] dark:bg-gray-800">
+                                    <Image
+                                        className="object-cover hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xs"
+                                        src={strapiImage(item?.image.url)}
+                                        alt={item?.image.alternativeText}
+                                        fill
+                                        sizes="100vw"
+                                    />
+                                </div>
+
+                                <div className="py-4 flex flex-col justify-between">
+                                    <p className="block mt-4 text-lg font-semibold text-gray-800 hover:underline dark:text-white md:text-xl">
+                                        Not a Guide to Integrating Strapi
+                                    </p>
+                                    <p className="text-left text-sm mt-2 text-muted md:text-base">
+                                        Looking to streamline your content your
+                                        your
+                                    </p>
+                                    <div className="flex space-x-2 items-center mt-4">
+                                        <p className="text-sm  uppercase">
+                                            category /
                                         </p>
-                                        <p>Jul 9, 2025</p>
+                                        <p className="text-neutral-500 text-sm max-w-xl group-hover:text-white transition duration-200">
+                                            September 11, 2024
+                                        </p>
                                     </div>
-                                </a>
-                            )
-                        })
-                    }
-                </div>
-            </div>
+                                </div>
+                            </a>
+                        </div>
+                    );
+                })}
 
+                {data.map((item, index) => {
+                    return (
+                        <div key={`div-${index}`} className="py-4">
+                            <a
+                                key={index}
+                                className="block focus:outline-hidden"
+                                href={`/article/${item.slug}`}
+                            >
+                                <div className="relative w-full overflow-hidden bg-gray-100 rounded-lg shadow aspect-[16/12] dark:bg-gray-800">
+                                    <Image
+                                        className="object-cover hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xs"
+                                        src={strapiImage(item?.image.url)}
+                                        alt={item?.image.alternativeText}
+                                        fill
+                                        sizes="100vw"
+                                    />
+                                </div>
+
+                                <div className="py-4 flex flex-col justify-between">
+                                    <p className="block mt-4 text-lg font-semibold text-gray-800 hover:underline dark:text-white md:text-xl">
+                                        Not a Guide to Integrating Strapi
+                                    </p>
+                                    <p className="text-left text-sm mt-2 text-muted md:text-base">
+                                        Looking to streamline your content your
+                                        your
+                                    </p>
+                                    <div className="flex space-x-2 items-center mt-4">
+                                        <p className="text-sm text-blue-500 uppercase">
+                                            category /
+                                        </p>
+                                        <p className="text-neutral-500 text-sm max-w-xl group-hover:text-white transition duration-200">
+                                            September 11, 2024
+                                        </p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    );
+                })}
+            </section>
         </div>
     );
 };
--- src/components/ArticleDetail.tsx
+++ src/components/ArticleDetail.tsx
@@ -1,41 +1,40 @@
-
-import { IArticle } from "@/lib/type";
-import Image from "next/image";
+import { IArticle } from '@/lib/type';
+import Image from 'next/image';
 import parse from 'html-react-parser';
-import { strapiImage } from "@/lib/strapiImage";
+import { strapiImage } from '@/lib/strapiImage';
 type Props = {
-    data: IArticle
-}
+    data: IArticle;
+};
 
 export const ArticleDetail = ({ data }: Props) => {
     return (
-        <div className="lg:mt-14">
-            <div className="text-center py-10 px-2 m-auto md:px-4 lg:max-w-3xl">
-                <h1 className="text-4xl pb-6">{ data.title }</h1>
-                <div className="flex items-center justify-center gap-x-5">
-                    <a className="inline-flex items-center gap-1.5 py-1 px-3 sm:py-2 sm:px-4 rounded-full text-xs sm:text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800" href="#">
-                    Company News
-                    </a>
-                    <p className="text-xs sm:text-sm text-gray-800 dark:text-neutral-200">January 18, 2023</p>
-                </div>
-
+       <div className="mt-24">
+                <article className="max-w-7xl mx-auto sm:mb-16">
+                    <header className="max-w-3xl mx-auto mb-6 sm:mb-8">
+                        <div className="mb-3 text-sm uppercase tracking-wider text-slate-500">
+                            <span className="text-primary">Featured</span> /
+                            <time>January 15, 2024</time>
+                        </div>
+                        <h1 className="text-3xl font-bold text-slate-900 sm:text-4xl md:text-5xl">
+                            Your Roadmap to Framework Selection Success
+                        </h1>
+                    </header>
+                    <figure className="w-full aspect-video relative my-10 ">
+                        <Image
+                            className="object-cover"
+                            src={strapiImage(data.image.url)}
+                            alt={data.image.alternativeText}
+                            fill
+                            sizes="100vw"
+                        />
+                    </figure>
+                    <div className="max-w-3xl mx-auto">
+                        <div className="prose prose-slate sm:prose-lg max-w-none">
+                            {parse(data.content)}
+                        </div>
+                    </div>
+                </article>
             </div>
-
-            <div className="relative w-full">
-                <Image
-                    src={strapiImage(data.image.url)}
-                    alt={data.image.alternativeText}
-                    width={1600} // 原始宽度（任意值，仅用于比例计算）
-                    height={900} // 原始高度（与宽度保持原始比例）
-                    layout="responsive"
-                />
-            </div>
-
-            <article className="text-4xl mx-auto md:px-4 lg:max-w-3xl">
-                {parse(data.content)}
-            </article>
-
-        </div>
     );
 };
 
